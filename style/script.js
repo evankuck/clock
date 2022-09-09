fc7c6ab2625626c82ed900077a75ac6990116dd7
evankuck <86078315+evankuck@users.noreply.github.com>
2022-09-09 11:29:14 -0400

    fixing jquery mistakes

--- style/script.js
+++ style/script.js
@@ -1,29 +1,21 @@
-function showTime(){
-    var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
-    var session = "AM";
-    
-    if(h == 0){
-        h = 12;
-    }
-    
-    if(h > 12){
-        h = h - 12;
-        session = "PM";
-    }
-    
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
-    
-    var time = h + ":" + m + ":" + s + " " + session;
-    document.getElementById("MyClockDisplay").innerText = time;
-    document.getElementById("MyClockDisplay").textContent = time;
-    
-    setTimeout(showTime, 1000);
-    
-}
+console.log("loaded");
 
-showTime();
+     function updateClock() {
+          var currentTime = new Date();
+
+          var currentHours = currentTime.getHours();
+          var currentMinutes = currentTime.getMinutes();
+          var currentSeconds = currentTime.getSeconds();
+
+          currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+          currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+
+          var amOrPm = (currentHours < 12) ? "AM" : "PM";
+
+          currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
+          currentHours = (currentHours === 0) ? 12 : currentHours;
+
+          var time = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + amOrPm;
+
+          $("#clock").html(time);
+     }
